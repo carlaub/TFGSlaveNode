9f5b111f88fc753a68695cb02d95d858993e528d
carlaub <carlaurreablazquez@gmail.com>
2018-07-15 19:19:33 +0200

    ResultQuery Modification

--- src/main/java/neo4j/ResultQuery.java
+++ src/main/java/neo4j/ResultQuery.java
@@ -10,19 +10,18 @@ import java.util.List;
  *
  */
 
-public class ResultQuery implements Serializable {
+public class ResultQuery implements Serializable{
 	String[] columnsName;
 	int columnsCount;
 	List<List<ResultEntity>> dataList;
 	int maxRowCount;
 
-	public ResultQuery(List<String> columnsName) {
-		this.columnsCount = columnsName.size();
+	public ResultQuery(String[] columnsName) {
+		this.columnsCount = columnsName.length;
 		this.maxRowCount = 0;
 
 		dataList = new ArrayList<>();
-		this.columnsName = new String[columnsCount];
-		this.columnsName = (String[]) columnsName.toArray();
+		this.columnsName = columnsName;
 
 		for (int i = 0; i < columnsCount; i++) {
 			dataList.add(new ArrayList<ResultEntity>());
