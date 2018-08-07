f637f406452d89fb8e7ee08edbc06a87645522b4
carlaub <carlaurreablazquez@gmail.com>
2018-08-07 20:54:47 +0200

    ResultQuery correction

--- src/main/java/neo4j/ResultEntity.java
+++ src/main/java/neo4j/ResultEntity.java
@@ -1,19 +1,13 @@
 package neo4j;
 
 import java.io.Serializable;
-import java.util.HashMap;
 
 /**
  * Created by Carla Urrea Blázquez on 25/06/2018.
  *
  */
 public abstract class ResultEntity implements Serializable {
-	protected HashMap<String, Object> properties;
-
 	public abstract String toString();
 
-	public ResultEntity() {
-
-	}
-
+	public ResultEntity() {}
 }
